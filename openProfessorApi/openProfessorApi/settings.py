95835a1d6198e53062096f06a9bb460117243d1c
Fernando Mattioli <mattioli.fernando@gmail.com>
2023-07-01 11:21:44 -0300

    Fix requirements for deployment on EL8

--- openProfessorApi/openProfessorApi/settings.py
+++ openProfessorApi/openProfessorApi/settings.py
@@ -146,6 +146,6 @@ CORS_ORIGIN_WHITELIST = (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 } 
